92a0ce4995e292cc452ad5857d716a2eaff80368
Benoît Rocco <benoit.rocco@gmail.com>
2020-05-30 19:06:32 +0200

    first script !

--- Scripting/SayHello.cs
+++ Scripting/SayHello.cs
@@ -3,7 +3,16 @@ using System;
 
 public class SayHello : Panel
 {
-	
+	public override void _Ready()
+	{
+		GetNode("Button")
+			.Connect("pressed", this, nameof(_OnButtonPressed));
+	}
+
+	public void _OnButtonPressed()
+	{
+		GetNode<Label>("Label").Text = "HELLO!";
+	}
 }
 
 
